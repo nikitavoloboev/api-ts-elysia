3394e1482329adcae50d35a10e98dce1d23ea745
Nikita <github@nikiv.dev>
2025-03-08 21:44:41 +0100

    .

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia"
 
-const app = new Elysia().get("/", "test").listen(process.env.PORT ?? 8787)
+const app = new Elysia().get("/", "test").listen(process.env.PORT ?? 4000)
 
 export type App = typeof app
 console.log(`Server is running at ${app.server?.hostname}:${app.server?.port}`)
